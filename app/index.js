428a29accb123cdb655efcddb15a3b284398abd1
Waleed Akram <waleedakram@Waleeds-MacBook-Pro.local>
2020-06-26 01:35:22 +0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const os = require('os');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    let data = os.networkInterfaces();
+    console.log(data.lo0[0]);
+    res.json(data.lo0[0]);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM node:8-alpine
+
+WORKDIR /home/node/app
+
+COPY ./app/package.json .
+
+COPY ./app/package-lock.json .
+
+RUN npm install --production
+
+COPY ./app/ .
+
+VOLUME [ "/home/node/app" ]
+
+EXPOSE 3000
+
+CMD [ "npm","start" ]
